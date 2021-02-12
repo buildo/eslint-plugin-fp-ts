a864859612a75253e7baee7c08a06aa29e547331
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-02-12 11:04:19 +0000

    Match project style more

--- src/rules/prefer-constructor.ts
+++ src/rules/prefer-constructor.ts
@@ -1,14 +1,7 @@
-import {
-  AST_NODE_TYPES,
-  TSESTree,
-} from "@typescript-eslint/experimental-utils";
-import { constant, constVoid, flow, pipe } from 'fp-ts/function';
-import { boolean, option, readonlyNonEmptyArray } from "fp-ts";
-import {
-  calleeIdentifier,
-  contextUtils,
-  createRule
-} from '../utils';
+import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/experimental-utils"
+import { boolean, option, readonlyNonEmptyArray } from "fp-ts"
+import { constVoid, flow, pipe } from "fp-ts/function"
+import { calleeIdentifier, contextUtils, createRule } from "../utils"
 
 export default createRule({
   name: "prefer-constructor",
@@ -19,30 +12,30 @@ export default createRule({
     docs: {
       category: "Best Practices",
       description: "afsafaf",
-      recommended: "warn",
+      recommended: "warn"
     },
     messages: {
       eitherFoldIsOptionFromEither: "cacsaffg",
-      replaceEitherFoldWithOptionFromEither: "dsagdgdsg",
-    },
+      replaceEitherFoldWithOptionFromEither: "dsagdgdsg"
+    }
   },
   defaultOptions: [],
   create(context) {
-    const { isIdentifierImportedFrom } = contextUtils(context);
+    const { isIdentifierImportedFrom } = contextUtils(context)
     return {
       CallExpression(node) {
         const isEitherFold = (node: TSESTree.CallExpression) => {
-            return pipe(
-              node,
-              calleeIdentifier,
-              option.filter((callee) => callee.name === "fold"),
-              option.chain(flow((callee) => callee.parent, option.fromNullable)),
-              option.filter((parent): parent is TSESTree.MemberExpression => parent.type === AST_NODE_TYPES.MemberExpression),
-              option.map((parent) => parent.object),
-              option.filter((object): object is TSESTree.Identifier => object.type === AST_NODE_TYPES.Identifier),
-              option.exists((object) => object.name === 'either')
-            );
-          }
+          return pipe(
+            node,
+            calleeIdentifier,
+            option.filter((callee) => callee.name === "fold"),
+            option.chain(flow((callee) => callee.parent, option.fromNullable)),
+            option.filter((parent): parent is TSESTree.MemberExpression => parent.type === AST_NODE_TYPES.MemberExpression),
+            option.map((parent) => parent.object),
+            option.filter((object): object is TSESTree.Identifier => object.type === AST_NODE_TYPES.Identifier),
+            option.exists((object) => object.name === "either")
+          )
+        }
 
         const isOptionNoneArrowFunctionExpression = (node: TSESTree.ArrowFunctionExpression) => {
           return pipe(
@@ -57,42 +50,41 @@ export default createRule({
           return pipe(
             node,
             option.of,
-            option.filter(() => node.object?.type === AST_NODE_TYPES.Identifier && node.object.name === 'option'),
-            option.filter(() => node.property?.type === AST_NODE_TYPES.Identifier && node.property.name === 'none'),
+            option.filter(() => node.object?.type === AST_NODE_TYPES.Identifier && node.object.name === "option"),
+            option.filter(() => node.property?.type === AST_NODE_TYPES.Identifier && node.property.name === "none"),
             option.isSome
           )
         }
 
-          const isOptionNoneCallExpression = (node: TSESTree.CallExpression) => {
-            return pipe(
-              node,
-              calleeIdentifier,
-              option.filter(
-                (callee) =>
-                  callee.name === "constant" &&
-                  isIdentifierImportedFrom(callee, /fp-ts\//, context)
-              ),
-              option.map(constant(node.arguments[0])),
-              option.chain(option.fromNullable),
-              option.filter((arg): arg is TSESTree.MemberExpression => arg.type === AST_NODE_TYPES.MemberExpression),
-              option.exists(isOptionNoneMemberExpression)
-            )
+        const isOptionNoneCallExpression = (node: TSESTree.CallExpression) => {
+          return pipe(
+            node,
+            calleeIdentifier,
+            option.filter(
+              (callee) =>
+                callee.name === "constant" &&
+                isIdentifierImportedFrom(callee, /fp-ts\//, context)
+            ),
+            option.chain(() => pipe(node.arguments[0], option.fromNullable)),
+            option.filter((arg): arg is TSESTree.MemberExpression => arg.type === AST_NODE_TYPES.MemberExpression),
+            option.exists(isOptionNoneMemberExpression)
+          )
+        }
+
+        const isOptionNone = (node: TSESTree.Expression) => {
+          switch (node.type) {
+            case AST_NODE_TYPES.ArrowFunctionExpression:
+              return isOptionNoneArrowFunctionExpression(node)
+            case AST_NODE_TYPES.CallExpression:
+              return isOptionNoneCallExpression(node)
+            case AST_NODE_TYPES.MemberExpression:
+              return isOptionNoneMemberExpression(node)
+            default:
+              return false
           }
+        }
 
-          const isOptionNone = (node: TSESTree.Expression) => {
-            switch (node.type) {
-              case AST_NODE_TYPES.ArrowFunctionExpression:
-                return isOptionNoneArrowFunctionExpression(node);
-              case AST_NODE_TYPES.CallExpression:
-                return isOptionNoneCallExpression(node);
-              case AST_NODE_TYPES.MemberExpression:
-                return isOptionNoneMemberExpression(node);
-              default:
-                return false;
-            }
-          };
-
-        const isOptionSomeValue =  (node: TSESTree.Expression) => {
+        const isOptionSomeValue = (node: TSESTree.Expression) => {
           return pipe(
             node,
             option.of,
@@ -100,15 +92,15 @@ export default createRule({
             option.filter((n): n is TSESTree.MemberExpression => n.type === AST_NODE_TYPES.MemberExpression),
             option.exists(
               (body) =>
-                body.object?.type === AST_NODE_TYPES.Identifier && body.object.name === 'option'
+                body.object?.type === AST_NODE_TYPES.Identifier && body.object.name === "option"
                 &&
-                body.property?.type === AST_NODE_TYPES.Identifier && body.property.name === 'some'
+                body.property?.type === AST_NODE_TYPES.Identifier && body.property.name === "some"
                 &&
                 (body.parent?.type !== AST_NODE_TYPES.CallExpression || body.parent.parent?.type !== AST_NODE_TYPES.ArrowFunctionExpression
-                ||
+                  ||
                   (body.parent.arguments.length === 1 && body.parent.parent.params.length === 1 && body.parent.arguments[0]?.type === AST_NODE_TYPES.Identifier && body.parent.parent.params[0]?.type === AST_NODE_TYPES.Identifier && body.parent.arguments[0].name === body.parent.parent.params[0].name))
-            ),
-          );
+            )
+          )
         }
         pipe(
           node,
@@ -123,7 +115,7 @@ export default createRule({
                 context.report({
                   loc: {
                     start: node.loc.start,
-                    end: node.loc.end,
+                    end: node.loc.end
                   },
                   messageId: "eitherFoldIsOptionFromEither",
                   suggest: [
@@ -134,17 +126,17 @@ export default createRule({
                           fixer.replaceTextRange(
                             node.range,
                             `option.fromEither`
-                          ),
-                        ];
-                      },
-                    },
-                  ],
-                });
+                          )
+                        ]
+                      }
+                    }
+                  ]
+                })
               })
             )
           )
-        );
-      },
-    };
-  },
-});
+        )
+      }
+    }
+  }
+})
